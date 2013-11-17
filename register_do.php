7db85a1082f75eae069ba3475cdc9c7cbbcf0f55
Yago A. Gonçalves <yagoag@gmail.com>
2013-11-17 00:02:33 -0200

    Passwords are now MD5 hashed

--- register_do.php
+++ register_do.php
@@ -8,17 +8,18 @@
 		$pass = $_POST['password'];
 
 		if(empty($user))
-			Message::show("Failed to register", "Type a username.<br /><a href=\"register.php\">Try again</a>.");
-		elseif(empty($pass))
-			Message::show("Failed to register", "Type a password.<br /><a href=\"register.php\">Try again</a>.");
+			Message::show("Failed to register", "Type a username.<br /><br /><a href=\"register.php\">Try again</a>.");
+		elseif(empty($_POST['password']))
+			Message::show("Failed to register", "Type a password.<br /><br /><a href=\"register.php\">Try again</a>.");
 		else {
 			$members = parse_ini_file("members.ini", true);
 			if(array_key_exists($user, $members))
-				Message::show("Failed to register", "The username you chose is already taken.<br /><a href=\"register.php\">Try again with another username.</a>");
+				Message::show("Failed to register", "The username you chose is already taken.<br /><br /><a href=\"register.php\">Try again with another username.</a>");
 			else {
+				$pass = md5($pass);
 				$member = new Member($user, $pass);
-				File::save_member($member);
-				Message::show("Registered successfully", "You've been successfully registered in the website.<br /><a href=\"login.php\">Click here to log in now.</a>");
+				File::save_new_member($member);
+				Message::show("Registered successfully", "You've been successfully registered in the website.<br /><br /><a href=\"login.php\">Click here to log in now.</a>");
 			}
 		}
 	}
