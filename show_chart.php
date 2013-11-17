fb5708de4e1bb5f2bd5a26e9dda0bc408e576a76
Yago A. Gonçalves <yagoag@gmail.com>
2013-11-17 19:34:55 -0200

    Updated show_chart.php
    show_chart.php should now show the charts just fine. Some CSS code and
    images have also been added.
    Actions not yet working because they depend on another file yet to be
    created, but links for it are there already.

--- show_chart.php
+++ show_chart.php
@@ -1,8 +1,26 @@
 <?php
 	include_once "protect.php";
+	include_once "classes/Project.php";
+
 	$name = $_SESSION['username'];
 	$group = $_GET['group'];
 	$project = $_GET['project'];
+
+	// Read content of projects.ini into an array
+	$projects = parse_ini_file("projects.ini", true);
+
+	// Create a Project object
+	$project_obj = new Project($project, $group);
+
+	// Add all the items of the project to it
+	foreach ($projects[$project]['todo'] as $item)
+		$project_obj->add_todo($item);
+
+	foreach ($projects[$project]['doing'] as $item)
+		$project_obj->add_doing($item);
+	
+	foreach ($projects[$project]['done'] as $item)
+		$project_obj->add_done($item);
 ?>
 
 <html>
@@ -30,54 +48,25 @@
 <div id="chart-container">
 	<!-- ToDo -->
 	<div id="todo">
-		<div class="title">To Do</div>
+		<div class="title"><?php echo "<a href=\"chart_do?project=\"" . $project . "&act=add_todo\">"; ?><input type="button" class="new-item" /></a> To Do</div>
 		<ul>
-			<li>An idea for a project will be carefully examined to determine whether
-				or not it benefits the organization. During this phase, a decision
-				making team will identify if the project can realistically be completed.
-				<br /><input type="button" class="button-back" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
-			<li>A project plan, project charter and/or project scope may be put in writing,
-				outlining the work to be performed. During this phase, a team should
-				prioritize the project, calculate a budget and schedule, and determine what
-				resources are needed.
-				<br /><input type="button" class="button-back" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
+			<?php $project_obj->print_todo(); ?>
 		</ul>
 	</div>
 
 	<!-- Doing -->
 	<div id="doing">
-		<div class="title">Doing</div>
+		<div class="title"><?php echo "<a href=\"chart_do?project=\"" . $project . "&act=add_doing\">"; ?><input type="button" class="new-item" /></a> Doing</div>
 		<ul>
-			<li>Resources' tasks are distributed and teams are informed of responsibilities.
-				This is a good time to bring up important project related information.
-				<br /><input type="button" class="button-back" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
-			<li>Project managers will compare project status and progress to the actual plan,
-				as resources perform the scheduled work. During this phase, project managers
-				may need to adjust schedules or do what is necessary to keep the project on
-				track.
-				<br /><input type="button" class="button-back" name="back_1" method="post" action="showChart.php" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
+			<?php $project_obj->print_doing(); ?>
 		</ul>
 	</div>
 
 	<!-- Done -->
 	<div id="done">
-		<div class="title">Done</div>
+		<div class="title"><?php echo "<a href=\"chart_do?project=\"" . $project . "&act=add_done\">"; ?><input type="button" class="new-item" /></a> Done</div>
 		<ul>
-			<li>After project tasks are completed and the client has approved the outcome, an
-				evaluation is necessary to highlight project success and/or learn from project
-				history.
-				<br /><input type="button" class="button-back" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
-			<li>Projects and project management processes vary from industry to industry;
-				however, these are more traditional elements of a project. The overarching
-				goal is typically to offer a product, change a process or to solve a problem
-				in order to benefit the organization.
-				<br /><input type="button" class="button-back" /> <input type="button" class="button-delete" /> <input type="button" class="button-next" />
-			</li>
+			<?php $project_obj->print_done(); ?>
 		</ul>
 	</div>
 </div>
