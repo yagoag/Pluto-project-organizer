bcc15e2a5300c91696925ea49f13d7ff9ecccae9
Yago A. Gonçalves <yagoag@gmail.com>
2013-11-17 02:19:01 -0200

    Fixed Message.php
    Now it doesn't print any more useless tabs and uses PHP's right end of
    line.

--- classes/Message.php
+++ classes/Message.php
@@ -2,22 +2,22 @@
 	class Message {
 		public static function show($title, $message) {
 			echo
-				'<!DOCTYPE html>
-					<head>
-						<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-						<title>' . $title . ' - Pluto</title>
-						<link href="style.css" media="all" rel="Stylesheet" type="text/css">
-					</head>
-
-					<body>
-					<div id="login-container">
-						<p class="title">' . $title . '</p>
-						<div id="login-form">
-							<p>' . $message . '</p>
-						</div>
-					</div>
-					</body>
-					</html>';
+				'<html>' . PHP_EOL .
+				'	<head>' . PHP_EOL .
+				'		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' . PHP_EOL .
+				'		<title>' . $title . ' - Pluto</title>' . PHP_EOL .
+				'		<link href="style.css" media="all" rel="Stylesheet" type="text/css">' . PHP_EOL .
+				'	</head>' . PHP_EOL .
+				PHP_EOL .
+				'	<body>' . PHP_EOL .
+				'	<div id="login-container">' . PHP_EOL .
+				'		<p class="title">' . $title . '</p>' . PHP_EOL .
+				'		<div id="message">' . PHP_EOL .
+				'			<p>' . $message . '</p>' . PHP_EOL .
+				'		</div>' . PHP_EOL .
+				'	</div>' . PHP_EOL .
+				'	</body>' . PHP_EOL .
+				'	</html>';
 		}
 	}
 ?>
