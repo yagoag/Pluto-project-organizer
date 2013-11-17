65930c662640c185984a3d73b85280b260bae849
Yago A. Gonçalves <yagoag@gmail.com>
2013-11-17 02:10:41 -0200

    Fixed index.php
    Redirects to show_groups.php instead of including it, fixing the bug in
    which the page would try to start the session twice.

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-	ob_start();
 	session_start();
 
-	if(!isset($_SESSION['user']) AND !isset($_SESSION['password']))
+	if(!isset($_SESSION['user']) && !isset($_SESSION['password']))
 		include_once "login.php";
 	else
-		include_once "show_groups.php";
+		header("Location: show_groups.php");
 ?>
